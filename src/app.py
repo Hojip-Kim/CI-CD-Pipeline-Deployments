333844d3bea2ea64910361a76f8025a55eccd30b
Hojip-Kim <rlaghwlq12@naver.com>
2025-05-15 10:12:32 +0900

    Adds ci github workflow

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ def details():
     return jsonify(
         {
             'time' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'hostname' : socket.gethostname()
+            'hostname' : socket.gethostname(), 
+            'message' : 'you are doing great, human'
         }
     )
 
