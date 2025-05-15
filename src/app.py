f7540e2e67d12df40131c19978e89d959a92c1dd
Hojip-Kim <rlaghwlq12@naver.com>
2025-05-15 19:14:44 +0900

    resource prune successfully

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
         {
             'time' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             'hostname' : socket.gethostname(),  
-            'message' : 'you are doing sgreat, human roll back deploy'
+            'message' : 'you are doing sgreat, human roll back prune'
         }
     )
 
