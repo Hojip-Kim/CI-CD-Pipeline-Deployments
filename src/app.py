74b751e7aed3d7f77f067f0ee832c609816ae55b
Hojip-Kim <rlaghwlq12@naver.com>
2025-05-15 19:59:17 +0900

    applcation yaml init

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
         {
             'time' : datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             'hostname' : socket.gethostname(),  
-            'message' : 'you are doing sgreat, human roll back prune'
+            'message' : 'you are doing sgreat, human roll back autosync'
         }
     )
 
